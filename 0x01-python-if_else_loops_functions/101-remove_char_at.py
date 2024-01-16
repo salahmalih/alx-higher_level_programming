78518ee42133603f64c6664d0285e7ebf0a44033
salahmalih <C130084552@taalim.ma>
2024-01-16 11:42:31 -0600

    task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""creates a copy of the string, removing the character at the position n."""
+
+def remove_char_at(str, n):
+    if 0 <= n < len(str):
+        return str[:n] + str[n+1:]
+    else:
+        return str
