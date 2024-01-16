0e60769216302b0e671c7c179e9ade910bdd663e
salahmalih <C130084552@taalim.ma>
2024-01-16 10:06:57 -0600

    fix some error

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""last digit of a number."""
+
+
+def print_last_digit(number):
+    if number < 0:
+        lastnumber = (number % -10) * -1
+    else:
+        lastnumber = number % 10
+    print("{}".format(lastnumber),end="")
+    return lastnumber
