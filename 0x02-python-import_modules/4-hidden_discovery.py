3ace58ed542070271d4c25881a8714945e85dc45
salahmalih <C130084552@taalim.ma>
2024-01-18 08:30:09 -0600

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
+
 if __name__ == "__main__":
-    """prints all the names defined by the compiled."""
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
     names = dir(hidden_4)
