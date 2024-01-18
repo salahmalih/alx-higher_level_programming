bae8d0c228386bfb46b0be19f10cf2630b2e7c61
salahmalih <C130084552@taalim.ma>
2024-01-18 07:45:21 -0600

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,10 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv) - 1
-    if count == 1:
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 arguments.")
     else:
         print("{} arguments:".format(count))
         for i in range(count):
