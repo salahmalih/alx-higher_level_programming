960209fc5ffa59ebab25fa3332410ff8b68763f5
salahmalih <C130084552@taalim.ma>
2024-01-20 18:26:46 -0600

    8. More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == None:
-        return (0, None)
-    return (len(sentence), sentence[0])
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
