e9b5c393e92c4e7df485d8df6d8f6ad2a0a77ca9
salahmalih <C130084552@taalim.ma>
2024-01-24 05:23:35 -0600

    2. Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    new_list = []
+    for i in range(len(my_list)):
+            if my_list[i] not in  new_list:
+                sum = sum + int(my_list[i])
+                new_list.append(my_list[i])
+    return sum
