38a3957ad7b7dc1f577e646067935bc6d33054b1
salahmalih <C130084552@taalim.ma>
2024-01-24 05:25:37 -0600

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 def uniq_add(my_list=[]):
     sum = 0
     new_list = []
-    for i in range(len(my_list)):
-            if my_list[i] not in  new_list:
-                sum = sum + int(my_list[i])
-                new_list.append(my_list[i])
+    for i in my_list:
+            if i not in  new_list:
+                sum = sum + int(i)
+                new_list.append(i)
     return sum
