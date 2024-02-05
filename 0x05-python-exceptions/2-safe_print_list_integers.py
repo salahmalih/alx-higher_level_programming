b9603234d704c9e0fd07c5e7410dc0dfb5fe34b1
salahmalih <C130084552@taalim.ma>
2024-02-05 07:54:32 -0600

    2 task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Print the first x elements of a list that are integers.
 
-Args:
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x elements of a list that are integers.
+
+    Args:
         my_list (list): The list to print elements from.
         x (int): The number of elements of my_list to print.
 
-Returns:
+    Returns:
         The number of elements printed.
-"""
-def safe_print_list_integers(my_list=[], x=0):
+    """
     num_printed = 0
     for i in range(0, x):
         try:
