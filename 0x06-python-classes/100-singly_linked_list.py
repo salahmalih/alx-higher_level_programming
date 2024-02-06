561b5af408f9440554afaae8a43cd764c87ee0d0
salahmalih <C130084552@taalim.ma>
2024-02-06 13:50:32 -0600

    task 7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,6 +2,7 @@
 
 """Define classes for a singly-linked list."""
 
+
 class Node:
     """Represent a node in a singly-linked list."""
 
@@ -12,15 +13,16 @@ class Node:
             data (int): The data of the new Node.
             next_node (Node): The next node of the new Node.
         """
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
-        return self.__data
+        """Get/set the data of the Node."""
+        return (self.__data)
+
     @data.setter
     def data(self, value):
-        """Get/set the data of the Node."""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
@@ -28,12 +30,14 @@ class Node:
     @property
     def next_node(self):
         """Get/set the next_node of the Node."""
-        return self.__next_node
+        return (self.__next_node)
+
     @next_node.setter
     def next_node(self, value):
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 
 class SinglyLinkedList:
     """Represent a singly-linked list."""
@@ -52,8 +56,7 @@ class SinglyLinkedList:
             value (Node): The new Node to insert.
         """
         new = Node(value)
-
-        if self.__head == None:
+        if self.__head is None:
             new.next_node = None
             self.__head = new
         elif self.__head.data > value:
@@ -61,7 +64,8 @@ class SinglyLinkedList:
             self.__head = new
         else:
             tmp = self.__head
-            while (tmp.next_node is not None and tmp.next_node.data < value):
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
                 tmp = tmp.next_node
             new.next_node = tmp.next_node
             tmp.next_node = new
