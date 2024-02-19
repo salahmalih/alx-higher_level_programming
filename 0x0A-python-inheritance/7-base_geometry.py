cc84513e402323772a66db850ca8098768007581
salahmalih <C130084552@taalim.ma>
2024-02-19 04:53:55 -0600

    all

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,5 +14,6 @@ class BaseGeometry:
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
