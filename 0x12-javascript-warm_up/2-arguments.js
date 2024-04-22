b103fa408f6dd91fe0e210722dbb227ac4071088
salahmalih <C130084552@taalim.ma>
2024-04-22 17:39:17 -0500

    all

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length > 2) { console.log('Arguments found'); } else { console.log('No argument'); }
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
