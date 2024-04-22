20ddab9b899a2a7b61ad257e8571d017e64ab8c5
salahmalih <C130084552@taalim.ma>
2024-04-22 14:00:24 -0500

    sec test

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2]) || parseInt(process.argv[2]) === 1) {
+if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  let i = 0;
-  let max = 0;
-  for (i = 0; i < process.argv.length - 1; i++) {
-    if (process.argv[i] < process.argv[i + 1]) {
-      max = process.argv[i + 1];
-    }
-  }
-  console.log(max);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
