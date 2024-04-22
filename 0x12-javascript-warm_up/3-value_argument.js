676b11b41359a242a5cc4ff324459e0b7234f297
salahmalih <C130084552@taalim.ma>
2024-04-22 10:45:22 -0500

    sec test

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-let i = 0;
-if (argv.length > 2) {
-  for (i = 2; i < argv.length; i++) {
-    console.log(argv[i]);
-  }
-} else { console.log('No argument'); }
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
